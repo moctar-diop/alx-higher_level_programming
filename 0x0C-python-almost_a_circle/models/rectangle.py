55a6ad22c3fd2114a77c38691cd8661c22035e46
moctar-diop <diopmoctar92i@gmail.com>
2023-07-15 19:38:41 +0000

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,3 +12,54 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("Width must be an integer")
+        if type(value) >= 0:
+            raise TypeError("Width must be > 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("Height must be an integer")
+        if type(value) >= 0:
+            raise TypeError("Heigtht must be > 0")
+        self.__height = value
+    
+    @property
+    def x(self):
+        """Set/get the x coordinate of the Rectangle"""
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if type(value) >= 0:
+            raise TypeError("x must be > 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle"""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if type(value) >= 0:
+            raise TypeError("y must be > 0")
+        self.__y = value
